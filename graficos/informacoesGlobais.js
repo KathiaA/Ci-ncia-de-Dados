36a86a3224699d4007f76426d1f0281c7d06402d
KathiaA <kathia.betim@escola.pr.gov.br>
2024-10-23 20:38:35 +0000

    adicionei o texto

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,8 +1,14 @@
-const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/educacao/educacao-dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados)
 }
+
+const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Podemos afirmar que no mundo existem cerca de <span> ${total_pessoas_mundo} </span> e desse total  <span> ${total_pessoas_com_acesso_a_educacao} </span> de pessoas tem acesso à educação. O tempo médio por dia que passam estudando é de <span> ${tempo_medio_dia_estudando} horas, já a quantidade de pessoas com ensino  superior é de aproximadamente <span> ${total_pessoas_com_educacao_superior} </span>`
+    const container = document.getElementById('graficos-container')
+container.appendChield(paragrafo):
 vizualizarInformacoesGlobais()
