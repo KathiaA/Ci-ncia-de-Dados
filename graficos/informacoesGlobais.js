00acc35243fdafb1fe6f99800e6cf386ba56121b
KathiaA <kathia.betim@escola.pr.gov.br>
2024-10-30 20:32:45 +0000

    formatei o texto

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,11 +4,10 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados)
-}
-
 const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Podemos afirmar que no mundo existem cerca de <span> ${total_pessoas_mundo} </span> e desse total  <span> ${total_pessoas_com_acesso_a_educacao} </span> de pessoas tem acesso à educação. O tempo médio por dia que passam estudando é de <span> ${tempo_medio_dia_estudando} horas, já a quantidade de pessoas com ensino  superior é de aproximadamente <span> ${total_pessoas_com_educacao_superior} </span>`
+    paragrafo.innerHTML = `Podemos afirmar que no mundo existem cerca de <span> ${dados.total_pessoas_mundo} </span> e desse total  <span> ${dados.total_pessoas_com_acesso_a_educacao} </span> de pessoas tem acesso à educação. O tempo médio por dia que passam estudando é de <span> ${dados.tempo_medio_dia_estudando} horas, já a quantidade de pessoas com ensino  superior é de aproximadamente <span> ${dados.total_pessoas_com_educacao_superior} </span>`
     const container = document.getElementById('graficos-container')
-container.appendChield(paragrafo):
+container.appendChild(paragrafo);
+}
 vizualizarInformacoesGlobais()
