c49c5dc356f5cda0a60e41497ee90e5a61aeda76
KathiaA <kathia.betim@escola.pr.gov.br>
2024-11-13 22:00:57 +0000

    Editei o gráfico

--- graficos/common.js
+++ graficos/common.js
@@ -1,5 +1,9 @@
 const getCSS = (variavel) => {
     return getComputedStyle(document.body).getPropertyValue(variavel)
-  };
+  }
+  color: getCSS('-primary-color'),
+size: 16,
+family: getCSS('--font')
+}
   
-  export {getCSS}
+  export {getCSS, tickConfig}
